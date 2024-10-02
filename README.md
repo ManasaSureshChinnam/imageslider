9581852d0d14e79a5ebc09af086f01b8b8151199
ManasaSureshChinnam <manasa.chinnam8@gmail.com>
2024-10-02 10:29:46 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# mypackage
+
+A Flutter widget for some Images.
+
+## Features
+
+- Showing boxing Images
+
+## Getting started
+
+To use this package, add mypackage as a dependency in your pubspec.yaml file.
+
+## Usage
+
+Minimal example:
+
+```dart
+     AddImages()
+```
---
+++ example/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:imageslider/imageslider.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  const Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Plugin Example"),
+      ),
+      body: const AddImages(),
+    );
+  }
+}
+
+
